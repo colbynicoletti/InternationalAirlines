9d81d5eca91f43962e7cab021e2781459e3370f0
ntgadomski <ntgadomski3779@eagle.fgcu.edu>
2021-03-06 20:11:53 -0500

    added CustomerAdderTest

---
+++ InternationalAirlines/src/CustomerAdderTest.java
@@ -0,0 +1,15 @@
+package InternationalAirlines.src;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CustomerAdderTest
+{
+    @Test
+    public void testDBCheckResponse()
+    {
+
+    }
+}
